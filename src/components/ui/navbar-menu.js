1e1acc08dfb9365a824498eb7cd8a6712943e361
Dianze Liu <dliu450@gatech.edu>
2026-08-24 20:13:19 -0700

    feat: add Personal Stock Deck project to navbar and update ProductItem to open links in new tabs

--- src/components/ui/navbar-menu.js
+++ src/components/ui/navbar-menu.js
@@ -60,7 +60,7 @@ export const Menu = ({ setActive, children }) => {
 
 export const ProductItem = ({ title, description, href, src }) => {
   return (
-    <Link href={href} className="flex space-x-2">
+    <Link href={href} target="_blank" rel="noopener noreferrer" className="flex space-x-2">
       <Image
         src={src}
         width={140}
